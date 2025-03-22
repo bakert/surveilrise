ef4f9e71e0d063f7f71676bc44e2c512ff70073e
Thomas David Baker <bakert@gmail.com>
2025-03-22 14:35:14 -0500

    Get a bit more serious about linting

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,12 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "next",
+    "turbo",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
   settings: {
     next: {
       rootDir: ["apps/*/", "packages/*/"],
@@ -7,8 +14,22 @@ module.exports = {
   },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "@typescript-eslint/no-unsafe-assignment": "error",
+    "@typescript-eslint/no-unsafe-member-access": "error",
+    "@typescript-eslint/no-unsafe-return": "error",
+    "@typescript-eslint/no-unsafe-call": "error",
   },
   parserOptions: {
+    project: [
+      "./tsconfig.json",
+      "./apps/*/tsconfig.json",
+      "./packages/*/tsconfig.json",
+    ],
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
