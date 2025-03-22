6b529a9d653bc02b2bdb196b1eeb6dbb2593aadc
Thomas David Baker <bakert@gmail.com>
2025-03-22 10:23:51 -0500

    Set a better default page size
    It's possible we should restrict to four cards wide as Scryfall does but this
    will give us a nice look in most sane browser widths, and shows more.

--- apps/web/src/app/api/search/search.service.ts
+++ apps/web/src/app/api/search/search.service.ts
@@ -19,7 +19,7 @@ export interface SearchParams {
 }
 
 export class SearchService {
-  private static readonly DEFAULT_PAGE_SIZE = 20;
+  private static readonly DEFAULT_PAGE_SIZE = 60; // 60 has 1,2, 3, 4, 5 and 6 as factors which means we get an even display up to 6 cards wide.
 
   static async search({ query, page, pageSize = this.DEFAULT_PAGE_SIZE }: SearchParams): Promise<SearchResult> {
     if (!query) {
