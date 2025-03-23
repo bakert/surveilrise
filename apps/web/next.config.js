bcddd033fad2927281ae4fad8a8bf06d5d66bec5
Thomas David Baker <bakert@gmail.com>
2025-03-22 19:03:58 -0500

    Account for errors.scryfall.com/soon.jpg

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,7 +3,7 @@ const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cards.scryfall.io'],
+    domains: ["cards.scryfall.io", "errors.scryfall.com"],
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
